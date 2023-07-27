623ad00bcbb2efd3757e60fa480a62d86ec90e80
Pratima <amitrana@MacUser.local>
2023-07-27 19:20:10 +0530

    Added segregartion for controller and logger files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { PORT } = require('./config');
+const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
- console.log(`Successfully started the server on PORT: ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+ console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+ Logger.info("Successfully started the server", "root", {})
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
+const { InfoController } = require('../../controllers');
+
 const router = express.Router();
 
-router.get('/info', (req, res)=> {
-    return res.json({msg: "ok"});
-})
+router.get('/info', InfoController.info);
 
 module.exports = router;
