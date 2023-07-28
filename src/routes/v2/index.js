572268aa3496a8cffa8a01f561c8bf3dbe36c375
Pratima <amitrana@MacUser.local>
2023-07-28 13:12:43 +0530

    Demo For API versioning

---
+++ src/routes/v2/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/info', (req, res) => {
+    return res.json({
+        msg: 'coming from v2 api'
+    })
+});
+
+module.exports = router;
